724129eb0f8abf7e33e24f12ccf4d9cd6fc09d4c
darkorockk <shadrin.dev@gmail.com>
2021-03-24 17:39:03 +0300

    2. Анализ доступности сервера. [#479183]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -8,21 +8,24 @@ public class Analizy {
             String[] tmp = {
                     "0", "0"
             };
-            PrintWriter out = new PrintWriter(new FileOutputStream(target));
-            while (in.ready()) {
-                String[] el = in.readLine().split(" ");
-                if (Integer.parseInt(el[0]) >= 400) {
-                    if (Integer.parseInt(tmp[0]) < 400) {
-                        tmp = el;
-                    }
-                } else {
-                    if (Integer.parseInt(tmp[0]) >= 400) {
-                        out.println(tmp[1] + ";" + el[1] + ";");
-                        tmp = el;
+            try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+                while (in.ready()) {
+                    String[] el = in.readLine().split(" ");
+                    if (Integer.parseInt(el[0]) >= 400) {
+                        if (Integer.parseInt(tmp[0]) < 400) {
+                            tmp = el;
+                        }
+                    } else {
+                        if (Integer.parseInt(tmp[0]) >= 400) {
+                            out.printf("%s;%s;%n", tmp[1], el[1]);
+                            tmp = el;
+                        }
                     }
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            out.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -30,7 +33,7 @@ public class Analizy {
 
     public static void main(String[] args) {
         Analizy an = new Analizy();
-        an.unavailable("/Users/darkorockk/IdeaProjects/job4j_design/server_log.txt",
-                "/Users/darkorockk/IdeaProjects/job4j_design/unavalable.txt");
+        an.unavailable("./server_log.txt",
+                "./unavalable.txt");
     }
 }
