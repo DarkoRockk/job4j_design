29ba2840426a5dcdb28e0a1ed95ce61ab25a42d7
darkorockk <shadrin.dev@gmail.com>
2021-03-24 17:43:04 +0300

    2. Анализ доступности сервера. [#479183]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -4,26 +4,23 @@ import java.io.*;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
             String[] tmp = {
                     "0", "0"
             };
-            try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-                while (in.ready()) {
-                    String[] el = in.readLine().split(" ");
-                    if (Integer.parseInt(el[0]) >= 400) {
-                        if (Integer.parseInt(tmp[0]) < 400) {
-                            tmp = el;
-                        }
-                    } else {
-                        if (Integer.parseInt(tmp[0]) >= 400) {
-                            out.printf("%s;%s;%n", tmp[1], el[1]);
-                            tmp = el;
-                        }
+            while (in.ready()) {
+                String[] el = in.readLine().split(" ");
+                if (Integer.parseInt(el[0]) >= 400) {
+                    if (Integer.parseInt(tmp[0]) < 400) {
+                        tmp = el;
+                    }
+                } else {
+                    if (Integer.parseInt(tmp[0]) >= 400) {
+                        out.printf("%s;%s;%n", tmp[1], el[1]);
+                        tmp = el;
                     }
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
 
         } catch (Exception e) {
