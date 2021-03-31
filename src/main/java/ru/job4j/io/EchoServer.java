f69bcfb3b9a444e0f3254b203ba20d5a532f1825
darkorockk <shadrin.dev@gmail.com>
2021-03-31 22:31:26 +0300

    1. Бот [#479199]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,4 +1,5 @@
 package ru.job4j.io;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,16 +13,21 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
+                    String response = "";
                     while (!(str = in.readLine()).isEmpty()) {
-                        if (str.contains("=Bye")) {
+                        if (str.contains("GET /?msg=Exit")) {
                             server.close();
                             out.write("Server closed successfully\r\n".getBytes());
                             return;
+                        } else if (str.contains("GET /?msg=Hello")) {
+                            response = "Hello";
+                        } else if (str.contains("GET /?msg=")) {
+                            response = "What";
                         }
                         System.out.println(str);
                     }
-                    out.write("HTTP/1.1 200 OK\r\n".getBytes());
-
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write((response + "\r\n\r\n").getBytes());
                 }
             }
         }
