7e96ebf9c3a22d804403b80715bd028eff3a8c26
darkorockk <shadrin.dev@gmail.com>
2021-04-26 17:49:25 +0300

    4. JAXB. Преобразование XML в POJO. [#479191]

--- src/main/java/ru/job4j/serialization/Animal.java
+++ src/main/java/ru/job4j/serialization/Animal.java
@@ -1,14 +1,33 @@
 package ru.job4j.serialization;
 
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Arrays;
 
+@XmlRootElement(name = "animal")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Animal {
-
+    @XmlAttribute
     private boolean sex;
+    @XmlAttribute
     private int age;
+    @XmlAttribute
     private String name;
+    @XmlElement
     private Vaccination vaсcine;
+    @XmlElementWrapper(name = "statuses")
+    @XmlElement(name = "status")
     private String[] statuses;
+
+    public Animal() {
+    }
 
     public Animal(boolean sex, int age, String name, Vaccination vaccine, String... statuses) {
         this.sex = sex;
@@ -27,5 +46,29 @@ public class Animal {
                 + ", vaсcine=" + vaсcine
                 + ", statuses=" + Arrays.toString(statuses)
                 + '}';
+    }
+
+    public static void main(String[] args) throws JAXBException, IOException {
+
+        Animal animal = new Animal(false, 6, "Jack", new Vaccination("Nobivak", 3.5), "RJCh", "RCh");
+
+
+        JAXBContext context = JAXBContext.newInstance(Animal.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml;
+        try (StringWriter writer = new StringWriter()) {
+
+            marshaller.marshal(animal, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        }
+
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            Animal result = (Animal) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
+
     }
 }
--- src/main/java/ru/job4j/serialization/Vaccination.java
+++ src/main/java/ru/job4j/serialization/Vaccination.java
@@ -1,13 +1,22 @@
 package ru.job4j.serialization;
 
 import java.io.Serializable;
+import com.sun.xml.txw2.annotation.XmlElement;
 
+import javax.xml.bind.annotation.XmlAttribute;
+
+@XmlElement(value = "vaccination")
 public class Vaccination implements Serializable {
     private static final long serialVersionUID = 1L;
+    @XmlAttribute
     private String drug;
-    private float version;
+    @XmlAttribute
+    private double version;
 
-    public Vaccination(String drug, float version) {
+    public Vaccination() {
+    }
+
+    public Vaccination(String drug, double version) {
         this.drug = drug;
         this.version = version;
     }
