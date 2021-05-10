18fa2b9d83fce20a1bccd43d42bb1b44071675a1
darkorockk <shadrin.dev@gmail.com>
2021-05-10 17:09:01 +0300

    Фильтры [#479110]

--- poduct_select.sql
+++ poduct_select.sql
@@ -5,3 +5,4 @@ select * from product where price = (select max(price) from product);
 select t.name, count(type_id) from product join type as t on type_id = t.id group by t.name;
 select * from product where type_id = 1 or type_id = 2;
 select t.name, count(type_id) from product join type as t on type_id = t.id group by t.name having count(type_id) < 10;
+select p.name, t.name from product as p join type as t on p.type_id = t.id;
