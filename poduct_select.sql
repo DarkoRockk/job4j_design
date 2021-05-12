f8d396e35b33d2d293ab8bf3442456f520479619
darkorockk <shadrin.dev@gmail.com>
2021-05-12 16:34:14 +0300

    Фильтры [#1732 #8363]

--- poduct_select.sql
+++ poduct_select.sql
@@ -1,6 +1,6 @@
 select * from product as p join type t on type_id = t.id where t.name = 'сыр';
 select * from product as p where p.name like '%мороженое%';
-select * from product as p where month(p.expired_date) = month(current_date + interval'1 month');
+select * from product as p where date_part('month', expired_date) = date_part('month', now() + interval '1 month');
 select * from product where price = (select max(price) from product);
 select t.name, count(type_id) from product join type as t on type_id = t.id group by t.name;
 select * from product as p join type t on type_id = t.id where t.name = 'сыр' or t.name = 'молоко';
