a42524b7941160817d771ab0f8d960a06d7cac13
darkorockk <shadrin.dev@gmail.com>
2021-05-12 19:48:24 +0300

    0.2. PrepareStatement [#479082]

---
+++ src/main/java/ru/job4j/jdbc/PrepareStatementDemo.java
@@ -0,0 +1,110 @@
+package ru.job4j.jdbc;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrepareStatementDemo {
+
+    public static void main(String[] args) throws Exception {
+        City city1 = new City(1, "Moscow", 15_000_000);
+        City city2 = new City(2, "Spb", 8_000_000);
+        City city3 = new City(3, "Kiev", 11_000_000);
+
+        PrepareStatementDemo psd = new PrepareStatementDemo();
+        psd.insert(city1);
+        psd.insert(city2);
+        //psd.insert(city3);
+        psd.update(city3);
+        psd.delete(2);
+        for (City city : psd.findAll()) {
+            System.out.println(String.format("%s %s %s", city.getId(), city.getName(), city.getPopulation()));
+        }
+
+
+    }
+
+    private Connection connection;
+
+    public PrepareStatementDemo() throws Exception {
+        initConnection();
+    }
+
+    public void initConnection() throws Exception {
+        Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://localhost:5432/idea_db";
+        String login = "postgres";
+        String password = "password";
+        connection = DriverManager.getConnection(url, login, password);
+    }
+
+    public void insert(City city) {
+        try (PreparedStatement statement =
+                     connection.prepareStatement("insert into cities(name, population) values (?, ?)")) {
+            statement.setString(1, city.getName());
+            statement.setInt(2, city.getPopulation());
+            statement.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public boolean update(City city) {
+        boolean result = false;
+        try (PreparedStatement statement =
+                     connection.prepareStatement("update cities set name = ?, population = ? where id = ?")) {
+            statement.setString(1, city.getName());
+            statement.setInt(2, city.getPopulation());
+            statement.setInt(3, city.getId());
+            result = statement.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    public boolean delete(int id) {
+        boolean result = false;
+        try (PreparedStatement statement =
+                     connection.prepareStatement("delete from cities where id = ?")) {
+            statement.setInt(1, id);
+            result = statement.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    public List<City> findAll() {
+        List<City> cities = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement("select * from cities")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    cities.add(new City(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getInt("population")
+                    ));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cities;
+    }
+//    public City insert(City city) {
+//        try (PreparedStatement statement =
+//                     connection.prepareStatement("insert into cities(name, population) values (?, ?)",
+//                             Statement.RETURN_GENERATED_KEYS)) {
+//            statement.setString(1, city.getName());
+//            statement.setInt(2, city.getPopulation());
+//            statement.execute();
+//            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+//                if (generatedKeys.next()) {
+//                    city.setId(generatedKeys.getInt(1));
+//                }
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return city;
+//    }
+
+}
